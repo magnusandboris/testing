2a0cb5e366972f0811be7e03159e6b9fd89631ee
magnusandboris <31415510+magnusandboris@users.noreply.github.com>
2017-10-15 16:01:06 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(600,600);
+  console.log("Setup");
+}
+
+function draw() {
+  background(255,0,255);
+  ellipse(300,300,frameCount/20,frameCount/20);
+  console.log(frameCount);
+}
